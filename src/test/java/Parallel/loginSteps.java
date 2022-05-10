80689ce584f83c7654abc76f5ec1a9e9bce04956
Aditya Parasher <parasharadi94@gmail.com>
2022-05-10 14:29:08 +0530

    Parallal execution using TestNG. Here we have created 2 folders for adding features into one and stepDefinitions, hooks along with a new Runner File into another folder.

---
+++ src/test/java/Parallel/loginSteps.java
@@ -0,0 +1,52 @@
+package Parallel;
+
+import com.factory.DriverFactory;
+import com.pages.loginPage;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;  //new import
+
+public class loginSteps {
+	
+	private static String title;
+	private loginPage loginPage = new loginPage(DriverFactory.getDriver());
+
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+		DriverFactory.getDriver().get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+	}
+
+	@When("user gets the title of the page")
+	public void user_gets_the_title_of_the_page() {
+		title = loginPage.getTitle();
+		System.out.println("Page title is: " + title);
+	}
+
+	@Then("page title should be {string}")
+	public void page_title_should_be(String expectedTitleName) {
+		Assert.assertTrue(title.contains(expectedTitleName));
+	}
+
+	@Then("forgot your password link should be displayed")
+	public void forgot_your_password_link_should_be_displayed() {
+		Assert.assertTrue(loginPage.isForgetPasswdLinkExists());
+	}
+
+	@When("user enters username {string}")
+	public void user_enters_username(String username) {
+		loginPage.enterUserName(username);
+	}
+
+	@When("user enters password {string}")
+	public void user_enters_password(String password) {
+		loginPage.enterPasswrd(password);
+	}
+
+	@When("user clicks on Login button")
+	public void user_clicks_on_login_button() {
+		loginPage.clickLoginBtn();
+	}
+
+}
