7ba27989b73f0f1ee48078fa36077bb439db0004
Aditya Parasher <parasharadi94@gmail.com>
2022-05-13 00:29:27 +0530

    Update the maven compiler plugin

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,58 +1,47 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "3923a329-6f77-4b78-9c83-4adfaa5d9dc8",
+    "id": "6a422b7e-7e11-4426-9716-322dce2c3b30",
     "feature": "Login page feature",
     "scenario": "Forgot Password link",
-    "start": 1652268292831,
-    "group": 22,
-    "content": "",
-    "tags": "",
-    "end": 1652268307389,
-    "className": "passed"
-  },
-  {
-    "id": "bf75a830-f409-4843-9dee-9ad9fd2c1100",
-    "feature": "Login page feature",
-    "scenario": "Login with correct credentials",
-    "start": 1652268292907,
-    "group": 23,
-    "content": "",
-    "tags": "",
-    "end": 1652268317668,
-    "className": "passed"
-  },
-  {
-    "id": "11a1b4e2-0b94-4857-90d8-0b50029247b0",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts page title",
-    "start": 1652268292908,
-    "group": 19,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1652268315754,
-    "className": "passed"
-  },
-  {
-    "id": "d86c28dd-3f32-48ff-84ea-845ba4f080e9",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts section count",
-    "start": 1652268292910,
-    "group": 20,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1652268318566,
-    "className": "failed"
-  },
-  {
-    "id": "e6cc7fad-1c64-406e-a314-11b906efc621",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1652268292908,
+    "start": 1652381800802,
     "group": 21,
     "content": "",
     "tags": "",
-    "end": 1652268307389,
+    "end": 1652381823932,
+    "className": "passed"
+  },
+  {
+    "id": "51429402-f029-44bf-94c5-b0286a0e9751",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1652381800891,
+    "group": 19,
+    "content": "",
+    "tags": "@accounts,@regression,",
+    "end": 1652381844958,
+    "className": "passed"
+  },
+  {
+    "id": "253568a9-8651-4e3c-b466-306d3ee4e738",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1652381800890,
+    "group": 20,
+    "content": "",
+    "tags": "",
+    "end": 1652381822965,
+    "className": "passed"
+  },
+  {
+    "id": "88ff7b78-b079-48bf-9916-c80b47407cb8",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1652381800889,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1652381843648,
     "className": "passed"
   }
 ]);
@@ -72,10 +61,6 @@ CucumberHTML.timelineGroups.pushArray([
   {
     "id": 22,
     "content": "Thread[TestNG-PoolService-3,5,main]"
-  },
-  {
-    "id": 23,
-    "content": "Thread[TestNG-PoolService-4,5,main]"
   }
 ]);
 });
